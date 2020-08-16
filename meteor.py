67167c4bf0f73282b955ee38da85f2421db63f2a
wildman082 <wildman@gmail.com>
2020-08-16 13:30:48 -0700

    added if __name__

--- meteor.py
+++ meteor.py
@@ -12,24 +12,22 @@ def calc_dist(lat1, lon1, lat2, lon2):
       math.sin( (lon2 - lon1) / 2 ) ** 2
     return 6372.8 * 2 * math.asin(math.sqrt(h))
 
-
-
-meteor_resp = requests.get("https://data.nasa.gov/resource/gh4g-9sfh.json")
-meteor_data = meteor_resp.json()
-
-my_lat = input("Enter your latitude: ")
-my_long = input("Enter your longitude: ")
-
-my_loc = (float(my_lat), float(my_long))
-
-for meteor in meteor_data:
-    if "reclat" in meteor and "reclong" in meteor: 
-        meteor["distance"] = calc_dist(float(meteor["reclat"]), float(meteor["reclong"]), my_loc[0], my_loc[1])
-
-
 def get_dist(meteor):
     return meteor.get("distance", math.inf)
 
-meteor_data.sort(key=get_dist)
+if __name__ == "__main__":
+    meteor_resp = requests.get("https://data.nasa.gov/resource/gh4g-9sfh.json")
+    meteor_data = meteor_resp.json()
 
-print(meteor_data[0])
+    my_lat = input("Enter your latitude: ")
+    my_long = input("Enter your longitude: ")
+
+    my_loc = (float(my_lat), float(my_long))
+
+    for meteor in meteor_data:
+        if "reclat" in meteor and "reclong" in meteor: 
+            meteor["distance"] = calc_dist(float(meteor["reclat"]), float(meteor["reclong"]), my_loc[0], my_loc[1])
+
+    meteor_data.sort(key=get_dist)
+
+    print(meteor_data[0])
